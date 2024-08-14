59c1c7cf5e682af1bfc455a2a6378934fee83039
Jon <jonathan.pepler@gmail.com>
2024-08-14 15:31:14 +0100

    show a 5 minute timer that resets when you click the page

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,20 +1,26 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import { useId, useState } from "react";
 import styles from "../page.module.css";
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { useKeyIncrementer } from "@/hooks/useKeyIncrementer";
 
 export default function Home() {
+  const [averageTime, setAverageTime] = useState(5 * 60);
+  const [timerKey, newTimer] = useKeyIncrementer("timer");
   return (
-    <main className={styles.main}>
-        <CountdownCircleTimer
-          isPlaying
-          duration={7}
-          colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-          colorsTime={[7, 5, 2, 0]}
-        >
-          {({ remainingTime }) => remainingTime}
-        </CountdownCircleTimer>
+    <main className={styles.main} onClick={newTimer}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={averageTime}
+        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+        colorsTime={[7, 5, 2, 0]}
+        key={timerKey}
+      >
+        {({ remainingTime }) =>
+          `${Math.floor(remainingTime / 60)}:${(remainingTime % 60).toString().padStart(2, "0")}`
+        }
+      </CountdownCircleTimer>
     </main>
   );
 }
