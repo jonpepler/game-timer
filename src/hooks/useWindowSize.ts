9296cb07cce277f0df494ce1699da7021ea913ed
Jon <jonathan.pepler@gmail.com>
2024-08-29 07:49:29 +0100

    fix: test for window in SSR

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,10 +1,18 @@
+"use client";
 import { useState, useEffect } from "react";
 
 const getWindowSize = () => {
-  const { innerWidth: width, innerHeight: height } = window;
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+  // Return default values for server-side rendering
   return {
-    width,
-    height,
+    width: 0,
+    height: 0,
   };
 };
 
@@ -12,12 +20,21 @@ export const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState(getWindowSize());
 
   useEffect(() => {
-    const handleResize = () => {
+    function handleResize() {
       setWindowSize(getWindowSize());
-    };
+    }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    // Add event listener only on client-side
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      handleResize();
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
   }, []);
 
   return windowSize;
