7b90473ff5065dc7fbd877f35df40e9228762e71
Jon Pepler <jonathan.pepler@gmail.com>
2026-03-04 14:57:23 +0000

    add node version and update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/game-timer'
+  output: "export",
+  basePath: "/game-timer",
 };
 
 module.exports = nextConfig;
